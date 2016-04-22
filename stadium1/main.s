107540017eb0e41ad5d90bf676938fa7622e3153
Guernouille <froggy25@hotmail.fr>
2016-04-22 15:57:59 +0200

    I forgot these tabs

--- stadium1/main.s
+++ stadium1/main.s
@@ -74,43 +74,43 @@
 ; PRNG
 .orga 0x359fa8
 	andi a0, v0, 0x0ff0             ; a0 = counter(cop0) & 0xff0
-	srl a0, a0, 0x4			        ; a0 >> 4
-	andi t6, a0, 0x00ff		        ; t6 = ((counter(cop0) & 0xff0) >> 4) & 0xff
-	blez t6, 0x0035a01c		        ; if negative number, jump to 0x35a01c
-	andi v1, a0, 0x00ff		        ; v1 = ((counter(cop0) & 0xff0) >> 4) & 0xff
+	srl a0, a0, 0x4                 ; a0 >> 4
+	andi t6, a0, 0x00ff             ; t6 = ((counter(cop0) & 0xff0) >> 4) & 0xff
+	blez t6, 0x0035a01c             ; if negative number, jump to 0x35a01c
+	andi v1, a0, 0x00ff             ; v1 = ((counter(cop0) & 0xff0) >> 4) & 0xff
 	lui a3, 0x0001
 	lui a2, 0x41c6
 	lui a1, 0x8439
 	lui a0, 0x8439
-	addiu a0, a0, 0xae40		    ; a0 is a pointer to ram (0x801ae1a0)
-	addiu a1, a1, 0xae3c		    ; a1 is a pointer to ram (0x801ae19c)
-	ori a2, a2, 0x4e6d		        ; a2 = 0x41c64e6d
-	ori a3, a3, 0x1020		        ; a3 = 0x11020
-	lw t7, 0x0000 (a1)		        ; t7 = (0x801ae19c)
-	lw v0, 0x0000 (a0)		        ; v0 = (0x801ae1a0)
-	addiu v1, v1, 0xffff		    ; decrease v1
-	multu t7, a2			        ; t7 × 0x41c64e6d
-	andi t0, v0, 0x0001		        ; is v0 odd ?
-	andi v1, v1, 0x00ff		        ; v1 & 0xff
-	xor t1, v0, a3			        ; t1 = (0x801ae1a0) ^ 0x11020
-	mflo t8				            ; t8 = t7 × 0x41c64e6d
-	addiu t9, t8, 0x303b		    ; t9 = t8 + 0x303b
-	beq t0, r0, 0x0035a010		    ; if v0 is even, jump to 0x35a010
-	sw t9, 0x0000 (a1)		        ; store seed × 0x41c64e6d + 0x303b to (0x801ae19c)
-	sw t1, 0x0000 (a0)		        ; store (0x801ae1a0) ^ 0x11020 to (0x801ae1a0)
-	or v0, t1, r0			        ; v0 = (0x801ae1a0) ^ 0x11020
-	srl t2, v0, 0x1			        ; t2 == v0 >> 1
-	bgtz v1, 0x00359fdc		        ; as long as v1 > 0, loop to 0x359fdc
-	sw t2, 0x0000 (a0)		        ; store v0 >> 1 to (0x801ae1a0)
+	addiu a0, a0, 0xae40            ; a0 is a pointer to ram (0x801ae1a0)
+	addiu a1, a1, 0xae3c            ; a1 is a pointer to ram (0x801ae19c)
+	ori a2, a2, 0x4e6d              ; a2 = 0x41c64e6d
+	ori a3, a3, 0x1020              ; a3 = 0x11020
+	lw t7, 0x0000 (a1)              ; t7 = (0x801ae19c)
+	lw v0, 0x0000 (a0)              ; v0 = (0x801ae1a0)
+	addiu v1, v1, 0xffff            ; decrease v1
+	multu t7, a2                    ; t7 × 0x41c64e6d
+	andi t0, v0, 0x0001             ; is v0 odd ?
+	andi v1, v1, 0x00ff             ; v1 & 0xff
+	xor t1, v0, a3                  ; t1 = (0x801ae1a0) ^ 0x11020
+	mflo t8                         ; t8 = t7 × 0x41c64e6d
+	addiu t9, t8, 0x303b            ; t9 = t8 + 0x303b
+	beq t0, r0, 0x0035a010          ; if v0 is even, jump to 0x35a010
+	sw t9, 0x0000 (a1)              ; store seed × 0x41c64e6d + 0x303b to (0x801ae19c)
+	sw t1, 0x0000 (a0)              ; store (0x801ae1a0) ^ 0x11020 to (0x801ae1a0)
+	or v0, t1, r0                   ; v0 = (0x801ae1a0) ^ 0x11020
+	srl t2, v0, 0x1                 ; t2 == v0 >> 1
+	bgtz v1, 0x00359fdc             ; as long as v1 > 0, loop to 0x359fdc
+	sw t2, 0x0000 (a0)              ; store v0 >> 1 to (0x801ae1a0)
 	lui a0, 0x8439
 	lui a1, 0x8439
-	addiu a1, a1, 0xae3c		    ; a1 is a pointer to ram (0x801ae19c)
-	addiu a0, a0, 0xae40		    ; a0 is a pointer to ram (0x801ae1a0)
-	lw t4, 0x0000 (a0)		        ; load pseudo-random number 1 in t4
-	lw t3, 0x0000 (a1)		        ; load pseudo-random number 2 in t3
+	addiu a1, a1, 0xae3c            ; a1 is a pointer to ram (0x801ae19c)
+	addiu a0, a0, 0xae40            ; a0 is a pointer to ram (0x801ae1a0)
+	lw t4, 0x0000 (a0)              ; load pseudo-random number 1 in t4
+	lw t3, 0x0000 (a1)              ; load pseudo-random number 2 in t3
 	lw ra, 0x0014 (sp)
 	addiu sp, sp, 0x0018
-	xor v1, t3, t4			        ; v1 = prn1 ^ prn2
+	xor v1, t3, t4                  ; v1 = prn1 ^ prn2
 	jr ra
-	andi v0, v1, 0x00ff		        ; v0 = (prn1 ^ prn2) & 0xff
+	andi v0, v1, 0x00ff             ; v0 = (prn1 ^ prn2) & 0xff
 .close
