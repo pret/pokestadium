3e6a50bf1a5f94bf08ace201119e277152d15100
tehzz <tehzz@users.noreply.github.com>
2022-12-30 12:15:14 -0500

    split intro_loader (2EC0)

---
+++ src/intro_loader.c
@@ -0,0 +1,75 @@
+#include <PR/ultratypes.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_800022C0.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002380.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002430.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_8000254C.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_800025C4.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002620.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002680.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002764.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002784.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002838.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002960.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002A14.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002A24.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("src/nonmatchings/intro_loader/func_80002A30.s")
+#endif
+#pragma GCC diagnostic pop
---
+++ src/nonmatchings/intro_loader/func_80002A24.s
@@ -0,0 +1,6 @@
+.section .text
+glabel func_80002A24
+/* 3624 80002A24 8C8EFFF4 */  lw         $t6, -0xC($a0)
+/* 3628 80002A28 03E00008 */  jr         $ra
+/* 362C 80002A2C 01C41023 */   subu      $v0, $t6, $a0
+
