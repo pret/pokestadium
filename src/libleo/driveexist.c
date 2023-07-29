63b0f8bb452a88340bd3c5e6d46cb9ed72ada3bb
LuigiBlood <luigiblood@gmail.com>
2023-07-29 21:37:18 +0200

    LeoDriveExist only checks for DDROM data, it's not a register

--- src/libleo/driveexist.c
+++ src/libleo/driveexist.c
@@ -5,7 +5,7 @@ void __osPiRelAccess(void);
 void __osPiGetAccess(void);
 
 #define CONNECTED 0x2129FFF8
-#define DDROM_STATUS_REG 0x06001010
+#define DDROM_ADDRESS_CHECK 0x06001010
 
 // https://github.com/LuigiBlood/64dd/wiki/64DD-IPL
 u32 LeoDriveExist(void) {
@@ -30,7 +30,7 @@ u32 LeoDriveExist(void) {
     HW_REG(PI_BSD_DOM1_RLS_REG, u32) = 3;
     HW_REG(PI_BSD_DOM1_PWD_REG, u32) = 0xFF;
 
-    ddromStatus = HW_REG(DDROM_STATUS_REG, u32);
+    ddromStatus = HW_REG(DDROM_ADDRESS_CHECK, u32);
 
     HW_REG(PI_BSD_DOM1_LAT_REG, u32) = lastLatency;
     HW_REG(PI_BSD_DOM1_PGS_REG, u32) = lastPageSize;
