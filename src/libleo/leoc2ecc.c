2168f435e91aa8da84c8f33ab8de0caf64485f45
LuigiBlood <luigiblood@gmail.com>
2023-08-03 15:22:05 +0200

    leoC2_Correction match (jtbl)

--- src/libleo/leoc2ecc.c
+++ src/libleo/leoc2ecc.c
@@ -3,11 +3,36 @@
 //Tables to add here
 extern u8 ganlog[512];
 extern u8 glog[512];
+//To be determined, should be a struct
+extern u8 LEOc2_param[16];
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunknown-pragmas"
 
+#ifdef NON_MATCHING
+s32 leoC2_Correction(void) {
+    switch (LEOc2_param[12]) {
+        case 1:
+            leoC2_single_ecc();
+            return 0;
+        case 2:
+            leoC2_double_ecc();
+            return 0;
+        case 3:
+            leoC2_3_ecc();
+            return 0;
+        case 4:
+            leoC2_4_ecc();
+            return 0;
+        default:
+            return -1;
+        case 0:
+            return 0;
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/libleo/leoc2ecc/leoC2_Correction.s")
+#endif
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libleo/leoc2ecc/leoC2_single_ecc.s")
 
