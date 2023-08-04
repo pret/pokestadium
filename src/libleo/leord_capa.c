e15f83961af3cd58f3a16b6d26fba38f2386f0ce
RevoSucks <projectrevotpp@hotmail.com>
2023-08-04 13:11:12 -0400

    more rodata work

--- src/libleo/leord_capa.c
+++ src/libleo/leord_capa.c
@@ -1,15 +1,6 @@
 #include <ultra64.h>
 #include "libleo/internal.h"
 
-// static?
-const u16 LEORAM_START_LBA[] = {
-    0x05A2, 0x07C6, 0x09EA, 0x0C0E, 0x0E32, 0x1010, 0x10DC,
-};
-// static?
-const s32 LEORAM_BYTE[] = {
-	0x024A9DC0, 0x01C226C0, 0x01450F00, 0x00D35680, 0x006CFD40, 0x001DA240, 0x00000000,
-};
-
 void leoRd_capacity(void) {
     if (LEOcur_command->header.control & LEO_CONTROL_WRT) {
         LEOcur_command->data.readwrite.lba = LEORAM_START_LBA[LEOdisk_type] - 0x18;
