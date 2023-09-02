168ed2e763d1aa0369045e7d4731f58118db1fc2
RevoSucks <projectrevotpp@hotmail.com>
2023-09-02 12:44:16 -0400

    begin clang-format work

--- src/3A80.c
+++ src/3A80.c
@@ -9,9 +9,9 @@
  * located whenever plugged in, but its only functional with the debug profiler.
  * It will, however, allocate a much bigger pool if set to non-0.
  */
-s32 gExpansionRAMStart = FALSE;
+u32 gExpansionRAMStart = FALSE;
 
-extern struct MainPool **gMainPool; // gMainPool
+extern struct MainPool** gMainPool; // gMainPool
 
 void func_80003860(void);
 s32 func_80007A58(void);
@@ -20,39 +20,45 @@ s32 func_80007A58(void);
  * Convert any valid address to its virtual (KSEG0) counterpart.
  */
 uintptr_t convert_addr_to_virt_addr(uintptr_t addr) {
-    uintptr_t retaddr = 0x00000000; // any invalid cases are treated as NULL return.
+    uintptr_t retaddr = NULL; // any invalid cases are treated as NULL return.
 
     // convert physical (in installed memory range) to virtual.
-    if (addr < (size_t) osMemSize) {
+    if (addr < osMemSize) {
         retaddr = addr | 0x80000000;
-    // convert segmented to virtual.
+        // convert segmented to virtual.
     } else if (addr < 0x10000000U) {
         retaddr = Memmap_GetSegmentVaddr(addr);
-    // convert a fragment pre-relocated address to a post-relocated virtual address.
-    } else if ((addr >= 0x81000000U) && (addr < 0x90000000U)) { // is the address in fragment space? convert it to its post-relocated address.
+        // convert a fragment pre-relocated address to a post-relocated virtual address.
+    } else if ((addr >= 0x81000000U) &&
+               (addr < 0x90000000U)) { // is the address in fragment space? convert it to its post-relocated address.
         retaddr = Memmap_GetFragmentVaddr(addr);
-    // pass-through addresses that are already virtual (in installed memory range)
-    } else if ((addr >= 0x80000000U) && (addr < (uintptr_t) (osMemSize + 0x80000000U))) {
+        // pass-through addresses that are already virtual (in installed memory range)
+    } else if ((addr >= 0x80000000U) && (addr < (uintptr_t)(osMemSize + 0x80000000U))) {
         retaddr = addr;
     }
 
     return retaddr;
 }
 
-/*
- * Copy memory from one address to the other. (why is this function not in HAL_libc?)
+/**
+ * Copy memory from one address to the other.
  */
 void HAL_Memcpy(u32* dest, u32* src, int size) {
-    while (size --> 0) {
+    while (size-- > 0) {
         *(dest++) = *(src++);
     }
 }
 
+// init_main_pools ?
 void func_80002F58(void) {
-    // wat? mem sizes are only ever 0x400000 or 0x800000. This check makes no sense.
-    // Effectively, it checks if the expansion RAM is in. But why not just use all
-    // of it, or at least do the correct check of osMemSize == 0x800000?
-    if ((gExpansionRAMStart != 0) && ((u32) osMemSize > 0x600000U)) {
+    /**
+     * wat? mem sizes are only ever 0x400000 or 0x800000. This check makes no sense
+     * in normal contexts. However, since osGetMemSize checks each MB at a time, if
+     * the 7MB check fails for whatever reason it can theoretically set osMemSize to
+     * 0x600000. This is not known to occur in practical contexts, but there may be
+     * some other unknown reason for this.
+     */
+    if ((gExpansionRAMStart > 0) && (osMemSize > 0x600000U)) {
         main_pool_init(&gPool, POOL_END_6MB);
     } else {
         main_pool_init(&gPool, POOL_END_4MB);
@@ -62,18 +68,20 @@ void func_80002F58(void) {
     gMainPool = mem_pool_try_init(0x10000, 0);
 }
 
-void *func_80002FDC(s32 size) {
+// main_malloc ?
+void* func_80002FDC(s32 size) {
     return mem_pool_alloc(gMainPool, size);
 }
 
-void func_80003004(void *arg0) {
+// main_free ?
+void func_80003004(void* arg0) {
     mem_pool_free(gMainPool, arg0);
 }
 
 void HAL_DrawRect(Gfx** dlist, s32 ulx, s32 lrx, u16 color) {
-    s32 uly = 0xF;
-    s32 lry = 0x11;
-    Gfx *gfx = *dlist;
+    s32 uly = 15;
+    s32 lry = 17;
+    Gfx* gfx = *dlist;
 
     if (func_80007A58() != 0) {
         ulx <<= 1;
@@ -88,54 +96,80 @@ void HAL_DrawRect(Gfx** dlist, s32 ulx, s32 lrx, u16 color) {
     *dlist = gfx;
 }
 
-void func_8000310C(Gfx** dlist) {
-    struct MainPool *pool = main_pool_get_pool();
-    s32 temp_s1 = main_pool_get_available() - gExpansionRAMStart;
+/**
+ * Render the memory profiler bar and print the MEM display.
+ */
+void profiler_draw_mem_display(Gfx** dlist) {
+    struct MainPool* pool = main_pool_get_pool(); // get pool pointer
+    /**
+     * Get the available memory offset by gExpansionRAMStart variable. This variable is weird; it
+     * seems to be used in reference to osMemSize checks >= 0x600000 which would only be true
+     * in Expansion RAM contexts. However, this game does not use the expansion RAM in any
+     * other context. This variable is negative when gExpansionRAMStart is too big, and not when
+     * it isnt. It is therefore reasonable to assume that this variable would represent how much
+     * memory is leftover and if negative would mean this cant fit into 4MB, which would therefore
+     * mean that the developers need to trim more memory to get it to fit into 4MB.
+     */
+    s32 available = main_pool_get_available() - gExpansionRAMStart;
 
-    if (temp_s1 >= 0) 
-    {
-        s32 base = 30;
-        s32 sp48 = ((u32) ( K0_TO_PHYS(pool->start)) >> 15) + base;
-        s32 sp44 = ((u32) ( K0_TO_PHYS(pool->listHeadL)) >> 15) + base;
-        s32 sp40 = ((u32) ( K0_TO_PHYS(pool->listHeadR) - gExpansionRAMStart) >> 15) + base;
-        s32 sp3C = ((u32) ( K0_TO_PHYS(pool->end) - gExpansionRAMStart) >> 15) + base;
+    if (available >= 0) {
+        // calculate approximation coordinates based on the current pool addresses in order
+        // to display them on the memory profiler.
+        s32 baseX = 30;
+        s32 startX = ((u32)(K0_TO_PHYS(pool->start)) >> 15) + baseX;
+        s32 headLX = ((u32)(K0_TO_PHYS(pool->listHeadL)) >> 15) + baseX;
+        s32 headRX = ((u32)(K0_TO_PHYS(pool->listHeadR) - gExpansionRAMStart) >> 15) + baseX;
+        s32 endX = ((u32)(K0_TO_PHYS(pool->end) - gExpansionRAMStart) >> 15) + baseX;
 
-        HAL_DrawRect(dlist, base, sp48, 0xFBCB);
-        HAL_DrawRect(dlist, sp48, sp44, 0xFFCB);
-        HAL_DrawRect(dlist, sp44, sp40, 0x2ABF);
-        HAL_DrawRect(dlist, sp40, sp3C, 0xFFCB);
-        HAL_Printf(base, 0x14, "MEM: +%XH (+%dK)",  temp_s1, temp_s1 / 1024);
-    }
-    else
-    {
-        s32 base = 30;
-        s32 sp34 = ((u32) ( K0_TO_PHYS(pool->start)) >> 15) + base;
-        s32 sp30 = ((u32) ( K0_TO_PHYS(pool->listHeadL)) >> 15) + base;
-        s32 sp2C = ((u32) ( K0_TO_PHYS(pool->listHeadR) - gExpansionRAMStart) >> 15) + base;
-        s32 sp28 = ((u32) ( K0_TO_PHYS(pool->end) - gExpansionRAMStart) >> 15) + base;
-        HAL_DrawRect(dlist, base, sp34, 0xFBCB);
-        HAL_DrawRect(dlist, sp34, sp2C, 0xFFCB);
-        HAL_DrawRect(dlist, sp2C, sp30, 0xF94B);
-        HAL_DrawRect(dlist, sp30, sp28, 0xFFCB);
-        HAL_Printf(base, 0x14, "MEM: -%XH (-%dK)", -temp_s1, -temp_s1 / 1024);
+        // draw the rects.
+        HAL_DrawRect(dlist, baseX, startX, GPACK_RGBA5551(248, 120, 40, 1));  // orange
+        HAL_DrawRect(dlist, startX, headLX, GPACK_RGBA5551(248, 248, 40, 1)); // yellow
+        HAL_DrawRect(dlist, headLX, headRX, GPACK_RGBA5551(40, 80, 248, 1));  // blue
+        HAL_DrawRect(dlist, headRX, endX, GPACK_RGBA5551(248, 248, 40, 1));   // yellow
+
+        // how many bytes and kilobytes are available?
+        HAL_Printf(baseX, 20, "MEM: +%XH (+%dK)", available, available / 1024);
+    } else {
+        // same as above.
+        s32 baseX = 30;
+        s32 startX = ((u32)(K0_TO_PHYS(pool->start)) >> 15) + baseX;
+        s32 headLX = ((u32)(K0_TO_PHYS(pool->listHeadL)) >> 15) + baseX;
+        s32 headRX = ((u32)(K0_TO_PHYS(pool->listHeadR) - gExpansionRAMStart) >> 15) + baseX;
+        s32 endX = ((u32)(K0_TO_PHYS(pool->end) - gExpansionRAMStart) >> 15) + baseX;
+
+        // draw the rects. if we are negative in the memory, we are using red for the backwards
+        // allocations to indicate too much memory is being used.
+        HAL_DrawRect(dlist, baseX, startX, GPACK_RGBA5551(248, 120, 40, 1));  // orange
+        HAL_DrawRect(dlist, startX, headRX, GPACK_RGBA5551(248, 248, 40, 1)); // yellow
+        HAL_DrawRect(dlist, headRX, headLX, GPACK_RGBA5551(248, 40, 40, 1));  // red
+        HAL_DrawRect(dlist, headLX, endX, GPACK_RGBA5551(248, 248, 40, 1));   // yellow
+
+        // how many bytes and kilobytes are available?
+        HAL_Printf(baseX, 20, "MEM: -%XH (-%dK)", -available, -available / 1024);
     }
 }
 
-/*
+/**
  * Clear memory address area.
  */
 void HAL_Memclear(u64* dest, u32 size) {
-    while( size --> 0 ) {
+    while (size-- > 0) {
         *(dest++) = -1;
     }
 }
 
-s32 func_80003348(u64* arg0)  {
+/**
+ * Unused function for checking some 64-bit range (possibly some display list?) for values
+ * cooresponding to 0x8040000080400000. This could have been for some memory test however
+ * the code for such a test is not present in this ROM, so we can only guess this function's
+ * intended usage.
+ */
+// check_stub_mem_area ?
+s32 func_80003348(u64* ptr) {
     s32 ret = 0;
 
-    while (*(arg0++) == 0x8040000080400000)
-    {   
+    while (*(ptr++) == 0x8040000080400000) {
         ret++;
     }
-    return ret * sizeof(*arg0);
+    return ret * sizeof(*ptr);
 }
