349be12d5964dfc36789e40b15b3bbb28b971ce9
ProjectRevoTPP <projectrevotpp@hotmail.com>
2024-08-05 21:25:06 -0400

    initial work on shifting

--- src/E1C0.c
+++ src/E1C0.c
@@ -1,7 +1,7 @@
 #include <ultra64.h>
 #include <PR/os_internal_reg.h>
 #include "dp_intro.h"
-#include "fragments.h"
+#include "sections.h"
 #include "memory.h"
 #include "dp_intro.h"
 #include "util.h"
@@ -31,10 +31,6 @@ extern struct UnkStruct800AA664* D_800AA664;
 extern char D_800AA668;
 
 extern u8 D_81200000[];
-
-// ROM offsets
-extern u8 D_3BA190[];
-extern u8 D_3CB130[];
 
 // from fragment 1
 void func_81206D9C(void);
@@ -90,7 +86,7 @@ void func_8000D738(struct UnkInputStruct8000D738* arg0) {
     D_800AA664 = main_pool_alloc(0x21E0, 0);
     func_80004454(((u32)((u32)&fragment1_TEXT_START & 0x0FF00000) >> 0x14) - 0x10, &fragment1_ROM_START,
                   &fragment1_ROM_END);
-    temp_v0 = func_800044F4(&D_3BA190, &D_3CB130, 1, 1);
+    temp_v0 = func_800044F4(&_3BA190_ROM_START, &_3CB130_ROM_START, 1, 1);
     D_800AA660->unk21FC = func_8000484C(temp_v0, 0);
     D_800AA660->unk2200 = func_8000484C(temp_v0, 1);
     D_800AA660->unk2204 = *arg0;
