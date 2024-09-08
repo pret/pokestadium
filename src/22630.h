4c2b949c20086c9dbeac2ed9b9934e65a3d9008d
Kelebek1 <eeeedddccc@hotmail.co.uk>
2024-09-08 22:10:20 +0100

    fragment35

--- src/22630.h
+++ src/22630.h
@@ -4,8 +4,10 @@
 #include "global.h"
 
 void func_80021CA4(char*, u8);
+void func_80023068(void);
 s32 func_8002311C(s32);
 void func_80024040(s16);
+void func_800240A0(void);
 
 
 #endif // _6BC0_H_
