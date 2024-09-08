385e58302b3410c466028a28bc6a3ee44eb4de83
Maide <you@example.com>
2024-09-08 22:10:20 +0100

    fragment35

---
+++ src/fragments/35/fragment35.h
@@ -0,0 +1,12 @@
+#ifndef _FRAGMENT35_H_
+#define _FRAGMENT35_H_
+
+#include "global.h"
+
+void func_82000020(void);
+s32 func_820002B4(UNUSED u8 arg0);
+void func_820002F0(u64 arg0);
+void func_8200036C(void);
+s32 func_820003A4(UNUSED s32 arg0, UNUSED s32 arg1);
+
+#endif // _FRAGMENT35_H_
