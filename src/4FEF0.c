29da6cc33fb59dc70cbeb560eec44c64570b64e4
ProjectRevoTPP <projectrevotpp@hotmail.com>
2024-09-14 20:11:25 -0400

    n_synaddplayer

--- src/4FEF0.c
+++
@@ -1,4 +0,0 @@
-#include "global.h"
-
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/4FEF0/func_8004F2F0.s")
---
+++ src/libnaudio/n_synaddplayer.c
@@ -0,0 +1,3 @@
+#include "global.h"
+
+#pragma GLOBAL_ASM("asm/us/nonmatchings/libnaudio/n_synaddplayer/n_alSynAddPlayer.s")
