740b043a17a09d0f8d2e6d8839c084436986e376
ProjectRevoTPP <projectrevotpp@hotmail.com>
2024-09-14 20:26:45 -0400

    n_synstartvoiceparam

--- src/50A00.c
+++ src/50A00.c
@@ -3,5 +3,3 @@
 #pragma GLOBAL_ASM("asm/us/nonmatchings/50A00/func_8004FE00.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/50A00/func_8004FF20.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/50A00/func_8004FF40.s")
