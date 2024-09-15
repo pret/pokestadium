feb2d0c9aa4728cddadee9d5f5123f7f8efab2ad
ProjectRevoTPP <projectrevotpp@hotmail.com>
2024-09-14 21:27:52 -0400

    n_env

--- src/4FF50.c
+++ src/4FF50.c
@@ -1,5 +1,5 @@
 #include "global.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/4FF50/func_8004F350.s")
+#pragma GLOBAL_ASM("asm/us/nonmatchings/4FF50/n_alResamplePull.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/4FF50/func_8004F4F8.s")
--- src/50120.c
+++ src/50120.c
@@ -2,6 +2,6 @@
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/50120/func_8004F520.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/50120/func_8004F95C.s")
+#pragma GLOBAL_ASM("asm/us/nonmatchings/50120/n_alLoadParam.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/50120/func_8004FB1C.s")
