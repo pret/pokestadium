dea1265a1514aac2818365a57cb692f5b2014ce6
Maide <you@example.com>
2024-09-23 15:36:17 +0100

    2E110

--- src/2E110.h
+++ src/2E110.h
@@ -4,10 +4,10 @@
 #include "global.h"
 
 void func_8002D510(void);
-s32 func_8002D5AC(s32);
-void func_8002D600(s32, s16);
-char* func_8002D7C0(char* arg0, s32 arg1, s32 arg2, s32 arg3);
-s32 func_8002D80C(char*);
+char* func_8002D5AC(s32);
+void func_8002D600(u32, s32);
+char* func_8002D7C0(char* arg0, s32 arg1, char** arg2, s32 arg3);
+s32 func_8002D80C(s8*);
 
 
 #endif // _2E110_H_
