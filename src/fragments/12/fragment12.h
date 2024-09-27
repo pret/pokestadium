10d561747f7a37a329afa5cde8ee865bd15042b8
Kelebek1 <eeeedddccc@hotmail.co.uk>
2024-09-27 15:43:00 +0100

    fragment12

---
+++ src/fragments/12/fragment12.h
@@ -0,0 +1,17 @@
+#ifndef _FRAGMENT12_H_
+#define _FRAGMENT12_H_
+
+#include "global.h"
+
+void func_86600020(void);
+void func_86600050(void);
+void func_866000FC(void);
+void func_86600264(s32 arg0);
+void func_86600438(s32 arg0);
+void func_8660048C(void);
+void func_866004F8(void);
+void func_866005B8(void);
+void func_86600658(void);
+s32 func_8660070C(UNUSED s32 arg0, UNUSED s32 arg1);
+
+#endif // _FRAGMENT12_H_
