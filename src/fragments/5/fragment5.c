623572c8c76cc72d57b6bb327136270768581582
Kelebek1 <eeeedddccc@hotmail.co.uk>
2024-09-27 15:19:57 +0100

    fragment5

--- src/fragments/5/fragment5.c
+++ src/fragments/5/fragment5.c
@@ -1,5 +1,31 @@
-#include "global.h"
+#include "fragment5.h"
+#include "src/20330.h"
+#include "src/controller.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/5/fragment5/func_87B00020.s")
+u16 D_87B000C0 = 0;
+u16 D_87B000C4 = 0x0069;
+u8 D_87B000C8 = 0;
+u8 D_87B000CC = 0;
+u8 D_87B000D0 = 0;
+u8 D_87B000D4 = 0;
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/5/fragment5/func_87B00050.s")
+unk_D_800AC870* D_87B000E0;
+unk_D_86002F30* D_87B000E4;
+unk_D_86002F34* D_87B000E8;
+unk_D_86002F34_00C* D_87B000EC;
+s16 D_87B000F0;
+s16 D_87B000F2;
+s16 D_87B000F4;
+
+void func_87B00020(void) {
+    Cont_StartReadInputs();
+    Cont_ReadInputs();
+    func_8001F730();
+}
+
+u32 func_87B00050(u32 arg0) {
+    if (arg0 == 0) {
+        return 0;
+    }
+    return ((guRandom() + osGetCount()) / 10) % arg0;
+}
