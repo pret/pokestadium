515b116f6fe5dbb9346d219559878d18bbe71f7d
Kelebek1 <eeeedddccc@hotmail.co.uk>
2025-04-16 21:06:24 +0100

    fragment51

--- src/fragments/51/fragment51.c
+++ src/fragments/51/fragment51.c
@@ -1,3 +1,7 @@
-#include "global.h"
+#include "fragment51.h"
+#include "src/29BA0.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/51/fragment51/func_82800020.s")
+s32 func_82800020(s32 arg0, s32 arg1) {
+    FRAGMENT_LOAD_AND_CALL(fragment33, 0, 0);
+    return 0;
+}
---
+++ src/fragments/51/fragment51.h
@@ -0,0 +1,8 @@
+#ifndef _FRAGMENT51_H_
+#define _FRAGMENT51_H_
+
+#include "global.h"
+
+s32 func_82800020(s32 arg0, s32 arg1);
+
+#endif // _FRAGMENT51_H_
