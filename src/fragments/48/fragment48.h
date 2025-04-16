99a98d32ba1a02450200daed0d7869ebc426bbb8
Kelebek1 <eeeedddccc@hotmail.co.uk>
2025-04-16 10:39:10 +0100

    fragment48

---
+++ src/fragments/48/fragment48.h
@@ -0,0 +1,19 @@
+#ifndef _FRAGMENT48_H_
+#define _FRAGMENT48_H_
+
+#include "global.h"
+
+void func_83900020(void);
+void func_83900028(void);
+void func_83900030(void);
+void func_83900038(void);
+void func_839000E4(void);
+void func_83900180(void);
+void func_839002B8(void);
+s32 func_839002F4(void);
+s32 func_839003DC(void);
+void func_83900484(void);
+void func_839004B4(void);
+s32 func_839004F4(s32 arg0, s32 arg1);
+
+#endif // _FRAGMENT48_H_
