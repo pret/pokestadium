643fe91ff08daec3a3fa04bc39a187a83a93607f
FantasmaShy <fantasmasaimon@gmail.com>
2025-07-07 23:51:00 -0400

    fragment30
    humble first try

--- src/fragments/30/fragment30.c
+++ src/fragments/30/fragment30.c
@@ -1,3 +1,7 @@
-#include "global.h"
+#include "fragment30.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/30/fragment30/func_88A00020.s")
+// unused wrapper?
+s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1) {
+    func_80029048((( (u32) &D_88000000 & 0x0FF00000) >> 0x14) - 0x10, fragment22_ROM_START, fragment23_ROM_START, 1, 0);
+    return 0x25;
+}
