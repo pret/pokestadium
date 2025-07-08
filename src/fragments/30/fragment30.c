a834c711aa2cc0b23d4aa4cc1b2686553914e393
shy <fantasmasaimon@gmail.com>
2025-07-08 15:46:05 -0400

    fragment30 fix

--- src/fragments/30/fragment30.c
+++ src/fragments/30/fragment30.c
@@ -1,7 +1,9 @@
-#include "fragment30.h"
+#include "src/29BA0.h"
 
-// unused wrapper?
+//  unused wrapper to load the pokelab?
+s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1);
+
 s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1) {
-    func_80029048((( (u32) &D_88000000 & 0x0FF00000) >> 0x14) - 0x10, fragment22_ROM_START, fragment23_ROM_START, 1, 0);
+    FRAGMENT_LOAD_AND_CALL2(fragment22, 1, 0);
     return 0x25;
 }
--- src/fragments/30/fragment30.h
+++
@@ -1,8 +0,0 @@
-#include "global.h"
-
-extern void func_80029048(s32 arg0, u8* arg1, u8* arg2, s32 arg3, s32 arg4);
-extern u8 D_88000000;
-extern u8 fragment22_ROM_START[];
-extern u8 fragment23_ROM_START[];
-
-s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1);
