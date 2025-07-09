1868051f55a15740cb0764b2cb318a3b1c9528b8
shy <fantasmasaimon@gmail.com>
2025-07-09 00:47:58 -0400

    fragment30 fix

--- src/fragments/30/fragment30.c
+++ src/fragments/30/fragment30.c
@@ -1,8 +1,8 @@
 #include "src/29BA0.h"
 
-//  unused wrapper to load the pokelab?
-s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1);
+#include "fragment30.h"
 
+//  unused wrapper to load the pokelab?
 s32 func_88A00020(UNUSED s32 arg0, UNUSED s32 arg1) {
     FRAGMENT_LOAD_AND_CALL2(fragment22, 1, 0);
     return 0x25;
