38bcdfdf66fb5b5f023605b0598bd4667c305690
Diamond Lewis <findlewis@gmail.com>
2025-09-11 09:28:30 -0500

    refactor

--- src/32D10.c
+++ src/32D10.c
@@ -193,6 +193,5 @@ void func_80032738(u32 arg0, u32 arg1) {
 }
 
 void func_800327CC(Gfx* arg0, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6, s32 arg7, s32 arg8, s32 arg9, s32 arg10, s32 arg11, s32 arg12, s32 arg13, s32 arg14, s32 arg15, s32 arg16) {
-    arg0->words.w0 = (s32) (((((arg1 & 0xF) << 0x14) | ((arg3 & 0x1F) << 0xF) | ((arg5 & 7) << 0xC) | ((arg7 & 7) << 9) | ((arg9 & 0xF) << 5) | (arg11 & 0x1F)) & 0xFFFFFF) | 0xFC000000);
-    arg0->words.w1 = (s32) (((u16)arg2 << 0x1C) | ((arg4 & 7) << 0xF) | ((arg6 & 7) << 0xC) | ((arg8 & 7) << 9) | ((arg10 & 0xF) << 0x18) | ((arg13 & 7) << 0x15) | ((arg15 & 7) << 0x12) | ((arg12 & 7) << 6) | ((arg14 & 7) * 8) | (arg16 & 7));
+    gDPSetCombine(arg0++, GCCc0w0(arg1, arg3, arg5, arg7) | GCCc1w0(arg9, arg11), GCCc0w1(arg2, arg4, arg6, arg8) | GCCc1w1(arg10, arg13, arg15, arg12, arg14, arg16));
 }
